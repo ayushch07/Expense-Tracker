bc7b8b99d3ecbf30b3f21c6b857fb4b43c6fa7b0
Ayush Chaturvedi <93607728+ayushch07@users.noreply.github.com>
2024-03-09 23:07:46 +0530

    Update seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -1,31 +1,31 @@
 
-const mongoose = require('mongoose');
-const Income = require('./models/IncomeModel');
+// const mongoose = require('mongoose');
+// const Income = require('./models/IncomeModel');
 
 
-const income = [
-    {
-        title:"Ayush Chaturvedi",
-        amount:12,
-        type:"income",
-        date:new Date(),
-        category:"Salary",
-        description:"Salary"
+// const income = [
+//     {
+//         title:"Ayush Chaturvedi",
+//         amount:12,
+//         type:"income",
+//         date:new Date(),
+//         category:"Salary",
+//         description:"Salary"
         
-    },
-    {
-        title:"Aman",
-        amount:120,
-        type:"race",
-        date:new Date(),
-        category:"Salary",
-        description:"Salary"
-    }
-]
+//     },
+//     {
+//         title:"Aman",
+//         amount:120,
+//         type:"race",
+//         date:new Date(),
+//         category:"Salary",
+//         description:"Salary"
+//     }
+// ]
 
-async function seedDB(){
-    await Income.insertMany(income);
-    console.log("data seeded successfully");
-}
+// async function seedDB(){
+//     await Income.insertMany(income);
+//     console.log("data seeded successfully");
+// }
 
-module.exports = seedDB;
+// module.exports = seedDB;
